230178e98ef095eb385f9ae269a9f061c01b0427
drrf <34131940+drrf@users.noreply.github.com>
2020-06-10 09:04:19 +0300

    update and check again
    new update 0.2

--- start.py
+++ start.py
@@ -2,19 +2,39 @@ import os
 from datetime import date
 from random import randrange
 
-rand = randrange(10+1)
+# setting
+num = 10
+rand = randrange(num) + 1
 today = date.today()
+folder = 'files'
+f_format = '.txt'
 
-# create folder to the files
-if not os.path.exists('files'):
-	os.makedirs('files')
+# create folder to the files, and delete old files
+def folder_and_files():
+	try:
+		if not os.path.exists(folder):
+			os.makedirs(folder)
+		for i in range(num):
+			file = './' + folder + '/' + str(i+1) + f_format
+			if os.path.exists(file):
+				os.remove(file)
+				print('remove: ' + file)
+	except:
+		print("except: folder_and_files()")
+		exit()
 
-for i in range(rand):
-	f = open("./files/" + str(i+1) + ".txt","w+")
-	f.write(str(today))
-	f.write("\nrand = %d\r\n" % (rand))
-	f.write("This is automation github script %d\r\n" % (i+1))
-	f.write("goodbye :)")
-	f.close
-	
-print("finish with {} new files!".format(rand))
+def create_new_files():
+	for i in range(rand):
+		f = open("./files/" + str(i+1) + f_format,"w+")
+		f.write(str(today))
+		f.write("\nrand = %d\r\n" % (rand))
+		f.write("This is automation github script %d\r\n" % (i+1))
+		f.write("goodbye :)")
+		f.close
+
+# program start from here
+folder_and_files()
+create_new_files()
+print("\nfinish with {} new files!".format(rand))
+
+
