e89041d2d5356aa1372838087fee091c4ae9da43
drrf <34131940+drrf@users.noreply.github.com>
2020-06-12 15:27:07 +0300

    init
    rewrite the script, now support github API

--- start.py
+++ start.py
@@ -1,47 +1,67 @@
 import os
 from datetime import date
+from github import Github
 from random import randrange
 
 '''
+	module:
+		https://github.com/PyGithub/PyGithub
+
 	todo:
 		0. add services in OS Cron or TS
 		1. run automatically random(1-10) times in each 24H
 		2. connect to git
 '''
 
-# setting
-num = 10
+# setting for github
+g = Github("user", "password")
+repo = g.get_repo('user/repo')
+
+# setting for script
+num = 5
 rand = randrange(num) + 1
 today = date.today()
 folder = 'files'
 f_format = '.txt'
 
-# create folder to the files, and delete old files
-def folder_and_files():
-	try:
-		if not os.path.exists(folder):
-			os.makedirs(folder)
-		for i in range(num):
-			file = './' + folder + '/' + str(i+1) + f_format
-			if os.path.exists(file):
-				os.remove(file)
-				print('remove: ' + file)
-	except:
-		print("except: folder_and_files()")
-		exit()
-
-def create_new_files():
+def remove_file_from_github():
+	for i in range(num):
+		try:
+			file = folder + '/' + str(i+1) + f_format
+			contents = repo.get_contents(file)
+			repo.delete_file(contents.path, "remove test", contents.sha)
+			print('remove from GitHub: ' + file)
+		except:
+			pass
+	
+		
+def create_new_file_github():
 	for i in range(rand):
-		f = open("./files/" + str(i+1) + f_format,"w+")
-		f.write(str(today))
-		f.write("\nrand = %d\r\n" % (rand))
-		f.write("This is automation github script %d\r\n" % (i+1))
-		f.write("goodbye :)")
-		f.close
+		file_data = create_data_in_file(i)
+		file = folder + '/' + str(i+1) + f_format
+		
+		try:
+			repo.create_file(file, "upload test", file_data)
+			print('create file: ' + file)
+		except:
+			pass
+			
+def create_data_in_file(i):
+	file_data = ''
+	file_data += str(today)
+	file_data += ("\nrand = %d\r\n" % (rand))
+	file_data += ("This is automation github script %d\r\n" % (i+1))
+	file_data += "goodbye :)"
+	
+	return file_data
+
+# for testing only	
+def github_get_repo_list():
+	for repo in g.get_user().get_repos():
+		print(repo.name)
+
 
 # program start from here
-folder_and_files()
-create_new_files()
+remove_file_from_github()
+create_new_file_github()
 print("\nfinish with {} new files!".format(rand))
-
-
